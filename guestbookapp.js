cb4321a724f0f6fc60ed225b61f432c01b0e71f6
BuluMatziel <43291490+BuluMatziel@users.noreply.github.com>
2020-03-17 11:00:49 +0200

    set view engine to ejs

--- guestbookapp.js
+++ guestbookapp.js
@@ -3,6 +3,9 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 5000;
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 // Require the module required for using form data
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
